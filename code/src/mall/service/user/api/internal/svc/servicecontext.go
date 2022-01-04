39de5c255245ae3985bbfd7e5faa8935bcb0ed01
bruce <1272542526@qq.com>
2022-01-04 11:14:29 +0800

    xg

---
+++ code/src/mall/service/user/api/internal/svc/servicecontext.go
@@ -0,0 +1,20 @@
+package svc
+
+import (
+	"github.com/tal-tech/go-zero/zrpc"
+	"mall/service/user/api/internal/config"
+	"mall/service/user/rpc/userclient"
+)
+
+type ServiceContext struct {
+	Config config.Config
+
+	UserRpc userclient.User
+}
+
+func NewServiceContext(c config.Config) *ServiceContext {
+	return &ServiceContext{
+		Config:  c,
+		UserRpc: userclient.NewUser(zrpc.MustNewClient(c.UserRpc)),
+	}
+}
