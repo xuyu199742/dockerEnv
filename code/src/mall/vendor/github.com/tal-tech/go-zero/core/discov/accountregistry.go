1236dbf77dc18797d6b84bdf86b68aadf5d06a48
bruce <1272542526@qq.com>
2022-01-05 12:11:50 +0800

    xg

--- code/src/mall/vendor/github.com/tal-tech/go-zero/core/discov/accountregistry.go
+++
@@ -1,7 +0,0 @@
-package discov
-
-import "github.com/tal-tech/go-zero/core/discov/internal"
-
-func RegisterAccount(endpoints []string, user, pass string) {
-	internal.AddAccount(endpoints, user, pass)
-}
